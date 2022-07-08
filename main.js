5458f6a4b72d31f0f5d0f23c6fbcac5a67ff638c
ingadi <axelingadi@gmail.com>
2022-07-08 17:09:28 +0300

    add App class render method

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
 import "./style.css";
+
+class App {
+  constructor(state, dom, template) {
+    this.state = state;
+    this.dom = dom;
+    this.template = template;
+  }
+
+  render() {
+    const tasksHTML = this.state
+      .reduce((html, task) => {
+        html.push(
+          `<li class="list__item">
+          <input
+            title="Mark done"
+            class="list__item-checkbox"
+            type="checkbox"
+            name="${task.name}"
+            id="${task.id}"
+          />
+          <span class="list__item-text">${task.name}</span>
+          <button
+            title="Delete task"
+            class="list__item-remove"
+            type="button"
+          ></button>
+        </li>`
+        );
+        return html;
+      }, [])
+      .join("");
+    this.dom.innerHTML = tasksHTML;
+  }
+}
+
+new App(
+  [
+    {
+      id: Date.now(),
+      name: "Task 1",
+    },
+    {
+      id: Date.now(),
+      name: "Task 2",
+    },
+    {
+      id: Date.now(),
+      name: "Task 3",
+    },
+    {
+      id: Date.now(),
+      name: "Task 4",
+    },
+  ],
+  document.querySelector(".list"),
+  document.querySelector("#task")
+).render();
